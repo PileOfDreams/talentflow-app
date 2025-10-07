56e059cadaef300135fd8eca718ca73cb8403c8a
PileOfDreams <abhisekbiswas003@gmail.com>
2025-10-07 13:05:13 +0530

    Configured MSW for production deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-// Entry point for our app
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -11,10 +9,13 @@ import ThemeProvider from './context/ThemeContext';
 const queryClient = new QueryClient();
 
 async function prepareApp() {
-  if (import.meta.env.DEV) {
-    const { worker } = await import('./mocks/browser.js');
-    await worker.start();
-  }
+  // we have to run MSW in both development AND production.
+  const { worker } = await import('./mocks/browser.js');
+  await worker.start({
+    // This is important for Netlify deployment
+    onUnhandledRequest: 'bypass',
+  });
+  
   await ensureSeedData();
 }
 
